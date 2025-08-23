3577d1890ab7c2dbc864873c3771d75a2c04e855
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2025-08-23 14:02:23 -0600

    feat: axum hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, extract::Path, routing::get};
+
+async fn greet() -> &'static str {
+    "Hello World!"
+}
+
+async fn greet_name(Path(name): Path<String>) -> String {
+    format!("Hello {name}!")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(greet))
+        .route("/{name}", get(greet_name));
+
+    let listner = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+
+    println!("Server running on http://127.0.0.1:8000");
+
+    axum::serve(listner, app).await.unwrap();
 }

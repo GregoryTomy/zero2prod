81faafc36af6e335cc02a960619b2d7b5c3bd8fb
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2025-08-31 12:16:30 -0600

    feat: initialize postgres db

---
+++ scripts/init_db.sh
@@ -0,0 +1,18 @@
+#!/opt/homebrew/bin/bash
+
+set -x
+set -eo pipefail
+
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_NAME:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5433}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
+docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000

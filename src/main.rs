43fbcdb438755bd2b0d3b7255561f147af0a8121
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2025-09-23 07:56:12 -0600

    Initializing telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use sqlx::PgPool;
 use tokio::net::TcpListener;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use zero2prod::configurations::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let configuration = get_configuration().expect("Failed to read configuration");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
--- src/startup.rs
+++ src/startup.rs
@@ -6,18 +6,20 @@ use axum::{
 };
 use sqlx::PgPool;
 use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
 
 pub fn create_app(db_pool: PgPool) -> Router {
     Router::new()
         .route("/health", get(health_check))
         .route("/subscriptions", post(subscribe))
+        .layer(TraceLayer::new_for_http())
         .with_state(db_pool)
 }
 
 pub async fn run(listener: TcpListener, db_pool: PgPool) -> Result<(), std::io::Error> {
     let app = create_app(db_pool);
 
-    println!("Server running on {}", listener.local_addr().unwrap());
+    tracing::info!("Server running on {}", listener.local_addr().unwrap());
 
     axum::serve(listener, app)
         .await

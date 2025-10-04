7b54e5bc24ce7a1b7bd811a4252ede94b11acd25
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2025-10-04 11:22:23 -0600

    clean up subcriber code into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use sqlx::PgPool;
 use tokio::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zero2prod::configurations::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subcriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
 
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    init_subcriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
 

cec45086005595ec05438d58bee759279a17c1b8
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2025-10-07 07:50:40 -0600

    Select configuration dynamically for local and prod

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,8 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let db_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+    let address = format!("127.0.0.1:{}", configuration.application.port);
+    let db_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to get configuration.");
 
     let listener = TcpListener::bind(&address)

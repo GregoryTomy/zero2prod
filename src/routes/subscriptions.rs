b2f106ebcc51bb0918374d0335cb5c3f48555b7c
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2025-10-12 13:30:24 -0600

    Add new subscirber

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,11 +3,41 @@ use serde::Deserialize;
 use sqlx::{PgPool, types::chrono::Utc};
 use uuid::Uuid;
 
+use crate::domain::{NewSubscriber, SubscriberName};
+
 #[allow(dead_code)]
 #[derive(Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in database",
+    skip(new_subscriber, pool)
+)]
+pub async fn insert_subsriber(
+    pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        insert into subscriptions (id, email, name, subscribed_at)
+        values ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
+        Utc::now(),
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(())
 }
 
 #[tracing::instrument(
@@ -20,33 +50,16 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(State(pool): State<PgPool>, Form(form): Form<FormData>) -> StatusCode {
-    match insert_subsriber(&pool, &form).await {
+    let new_subscriber = NewSubscriber {
+        email: form.email,
+        name: SubscriberName::parse(form.name),
+    };
+
+    match insert_subsriber(&pool, &new_subscriber).await {
         Ok(_) => {
             tracing::info!("New subscriber details have been saved");
             StatusCode::OK
         }
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
-}
-
-#[tracing::instrument(name = "Saving new subscriber details in database", skip(form, pool))]
-pub async fn insert_subsriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        r#"
-        insert into subscriptions (id, email, name, subscribed_at)
-        values ($1, $2, $3, $4)
-        "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now(),
-    )
-    .execute(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
-
-    Ok(())
 }
